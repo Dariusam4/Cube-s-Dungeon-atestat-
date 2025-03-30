1f107004589000aa9c9ce78b13b3f4729a2ddc96
Dariusam4 <ancadarius123@gmail.com>
2025-03-30 20:07:33 +0300

    Add Elements

---
+++ Cube's Dungeon/Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    float horizontalInput;
+    float moveSpeed = 5f;
+
+    bool isFacingRight = true;
+
+    float jumpPower = 6.7f;
+    bool isJumping = false;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        FlipSprite();
+
+        if(Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            isJumping = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
+    }
+    void FlipSprite()
+    {
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
+    }
+}
